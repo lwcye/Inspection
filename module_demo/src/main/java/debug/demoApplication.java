490dafe20d08e8f2b65de3bb3adc02002c8fbc49
caoliewei <15213024302@139.com>
2017-11-07 23:06:35 +0800

    配置module的gradle

---
+++ module_demo/src/main/java/debug/demoApplication.java
@@ -0,0 +1,17 @@
+package debug;
+
+import android.app.Application;
+
+/**
+ * <p>DESCRIBE</p><br>
+ *
+ * @author lwc
+ * @date 2017/11/7 21:42
+ * @note -
+ * -------------------------------------------------------------------------------------------------
+ * @modified -
+ * @date -
+ * @note -
+ */
+public class demoApplication extends Application{
+}
